f4f0cf65c44f0b204ab8e333ca7e0d4a6bcc55a5
Natsu-Akatsuki <1024756993@qq.com>
2022-05-07 20:11:15 +0800

    feat: 修改伪彩色部分的代码

--- amUtils/pointcloud_vis/intensity_to_color.py
+++ amUtils/pointcloud_vis/intensity_to_color.py
@@ -1,13 +1,19 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from numba import vectorize, float32, int32
 from funcy import print_durations
 import open3d as o3d
+
+"""
+note: pcd format: float32
+open3d format: float32[N,3]
+"""
 
 
 # @print_durations(unit='ms')
 @vectorize([float32(float32),
             float32(int32)])
-def intensity2rgb_pcd(intensity):
+def intensity_to_color_pcd(intensity):
     if intensity < 30:
         green = int(intensity / 30 * 255)
         red = 0
@@ -32,7 +38,7 @@ def intensity2rgb_pcd(intensity):
     return hex_color.view(np.float32)
 
 
-def pcd_color_to_o3d_color(pcd_color):
+def color_pcd_to_color_o3d(pcd_color):
     pcd_color = pcd_color.view(np.uint32)
     red = (pcd_color >> 16) / 255.0
     green = ((pcd_color >> 8) & 255) / 255.0
@@ -40,44 +46,53 @@ def pcd_color_to_o3d_color(pcd_color):
     return np.hstack([red[:, None], green[:, None], blue[:, None]])
 
 
-def generate_pcd(pointcloud, filename="sample.pcd", format="binary"):
-    with open(filename, 'w') as f:
-        f.write("# .PCD v.7 - Point Cloud Data file format\n")
-        f.write("VERSION .7\n")
-        f.write("FIELDS x y z rgb\n")
-        f.write("SIZE 4 4 4 4\n")
-        f.write("TYPE F F F F\n")
-        f.write("COUNT 1 1 1 1\n")
-        f.write("WIDTH {}\n".format(pointcloud.shape[0]))
-        f.write("HEIGHT 1\n")
-        f.write("VIEWPOINT 0 0 0 1 0 0 0\n")
-        f.write("POINTS {}\n".format(pointcloud.shape[0]))
-        if format == "ascii":
-            f.write("DATA ascii\n")
-            for i in range(pointcloud.shape[0]):
-                f.write(
-                    str(pointcloud[i][0]) + " " + str(pointcloud[i][1]) + " " + str(pointcloud[i][2]) + " " + str(
-                        pointcloud[i][3]) + "\n")
-        elif format == "binary":
+def intensity_to_color_o3d(intensity, is_normalized):
+    if is_normalized:
+        normalized_min = np.min(intensity)
+        normalized_max = np.max(intensity)
+    else:
+        normalized_min = 0.0
+        normalized_max = 255.0
+    # [:,:3]: remove the alpha channel
+    color_o3d = plt.get_cmap('jet')((intensity - normalized_min) / (normalized_max - normalized_min))[:, :3]
+    return color_o3d
+
+
+if __name__ == '__main__':
+    def save_pcd(pointcloud, filename="sample.pcd"):
+        with open(filename, 'w') as f:
+            f.write("# .PCD v.7 - Point Cloud Data file format\n")
+            f.write("VERSION .7\n")
+            f.write("FIELDS x y z rgb\n")
+            f.write("SIZE 4 4 4 4\n")
+            f.write("TYPE F F F F\n")
+            f.write("COUNT 1 1 1 1\n")
+            f.write("WIDTH {}\n".format(pointcloud.shape[0]))
+            f.write("HEIGHT 1\n")
+            f.write("VIEWPOINT 0 0 0 1 0 0 0\n")
+            f.write("POINTS {}\n".format(pointcloud.shape[0]))
             f.write("DATA binary\n")
             pointcloud.tofile(f)
 
 
-if __name__ == '__main__':
-    # 对点云的强度信息进行线性变换，得到颜色信息
-    pointcloud = np.load("../data/livox_pointcloud.npy")
-    # 得到rviz/ros/pcd的颜色信息(float32)
-    pointcloud[:, 3] = intensity2rgb_pcd(pointcloud[:, 3])
-    generate_pcd(pointcloud, filename="sample.pcd", format="binary")
+    pointcloud = np.load("data/stack_pointcloud.npy")[:, :4]
+    pointcloud = np.array(pointcloud, dtype=np.float32)
+    intensity = pointcloud[:, 3].copy()
+
+    # achieve rviz/ros/pcd-format (float32) color
+    color_pcd = intensity_to_color_pcd(pointcloud[:, 3])
+    pointcloud[:, 3] = color_pcd
+    save_pcd(pointcloud, filename="data/output.pcd")
 
     pointcloud_o3d = o3d.geometry.PointCloud()
     pointcloud_o3d.points = o3d.utility.Vector3dVector(pointcloud[:, 0:3])
-    # 得到open3d的颜色信息(float32(3))
-    color_o3d = pcd_color_to_o3d_color(pointcloud[:, 3])
+
+    # color_o3d = color_pcd_to_color_o3d(color_pcd)
+    color_o3d = intensity_to_color_o3d(intensity, is_normalized=True)
     pointcloud_o3d.colors = o3d.utility.Vector3dVector(color_o3d)
     vis = o3d.visualization.Visualizer()
     vis.create_window()
     vis.add_geometry(pointcloud_o3d)
-    vis.get_render_option().point_size = 2
+    vis.get_render_option().point_size = 1
     vis.run()
     vis.destroy_window()
