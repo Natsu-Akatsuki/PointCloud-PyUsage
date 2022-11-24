67efe2f5b211610c75b2f694d32360bd7cd4057f
Natsu-Akatsuki <1024756993@qq.com>
2022-11-24 09:33:35 +0800

    refactor: 避免每次都显示日志

--- ampcl/filter/pointcloud_filter_pyb.py
+++ ampcl/filter/pointcloud_filter_pyb.py
@@ -1,8 +1,10 @@
 from .filter_pyb import cVoxelFilter
 
 
-def c_voxel_filter(pointcloud, voxel_size=(0.05, 0.05, 0.05)):
-    print(f"[Filtered] Before down sample the point cloud size is {pointcloud.shape[0]}")
+def c_voxel_filter(pointcloud, voxel_size=(0.05, 0.05, 0.05), log=False):
+    if log:
+        print(f"[Filtered] Before down sample the point cloud size is {pointcloud.shape[0]}")
     filtered_pointcloud = cVoxelFilter(pointcloud, voxel_size=voxel_size)
-    print(f"[Filtered] Before down sample the point cloud size is {filtered_pointcloud.shape[0]}")
+    if log:
+        print(f"[Filtered] Before down sample the point cloud size is {filtered_pointcloud.shape[0]}")
     return filtered_pointcloud
