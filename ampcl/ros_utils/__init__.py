9e520b66fd94b303b0ddf9bbfd7bf0c806078300
Natsu-Akatsuki <1024756993@qq.com>
2023-02-02 02:58:31 +0800

    fix: 缺位置参数

--- ampcl/ros_utils/__init__.py
+++ ampcl/ros_utils/__init__.py
@@ -15,21 +15,21 @@ except:
 
 def pointcloud2_to_np(pointcloud2, remove_nans=True, field="xyzi"):
     if field == "xyz":
-        return pointcloud2_to_xyz_array(pointcloud2, remove_nans=remove_nans)
+        return pointcloud2_to_xyz_array(pointcloud2, remove_nans=remove_nans).astype(np.float32)
     elif field == "xyzi":
-        return pointcloud2_to_xyzi_array(pointcloud2, remove_nans=remove_nans)
+        return pointcloud2_to_xyzi_array(pointcloud2, remove_nans=remove_nans).astype(np.float32)
     else:
         raise NotImplementedError("field: {} isn't implemented!".format(field))
 
 
-def np_to_pointcloud2(pointcloud_np, field="xyzi"):
+def np_to_pointcloud2(pointcloud_np, header, field="xyzi"):
     if field == "xyz":
-        return xyz_numpy_to_pointcloud2(pointcloud_np)
+        return xyz_numpy_to_pointcloud2(pointcloud_np, header=header)
     elif field == "xyzi":
-        return xyzi_numpy_to_pointcloud2(pointcloud_np)
+        return xyzi_numpy_to_pointcloud2(pointcloud_np, header=header)
     elif field == "xyzrgb":
-        return xyzrgb_numpy_to_pointcloud2(pointcloud_np)
+        return xyzrgb_numpy_to_pointcloud2(pointcloud_np, header=header)
     elif field == "xyzirgb":
-        return xyzirgb_numpy_to_pointcloud2(pointcloud_np)
+        return xyzirgb_numpy_to_pointcloud2(pointcloud_np, header=header)
     else:
         raise NotImplementedError("field {} isn't implemented!".format(field))
