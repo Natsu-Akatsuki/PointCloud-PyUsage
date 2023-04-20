be0b06ac9556cca26d020c3e416a33ec08dd6243
Natsu-Akatsuki <1024756993@qq.com>
2023-04-20 17:03:20 +0800

    bug：修订目录

--- ampcl/perception/__init__.py
+++ ampcl/perception/__init__.py
@@ -1,3 +1,3 @@
 from .ground_segmentation_pybind import ground_segmentation_ransac, \
     ground_segmentation_gpf
-from euclidean_cluster_pyb import cEuclideanCluster
+from .euclidean_cluster_pyb import cEuclideanCluster
